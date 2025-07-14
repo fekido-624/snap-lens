f8872f38cd40944646a88accb4f05a53982f9464
unknown <amirulhafizasmadi@gmail.com>
2025-07-14 08:51:10 +0800

    update camera belakang

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const video = document.getElementById('camera');
+const video = document.getElementById("camera");
 
-navigator.mediaDevices.getUserMedia({ video: true })
+const constraints = {
+  video: {
+    facingMode: { ideal: "environment" } // Kamera belakang
+  },
+  audio: false
+};
+
+navigator.mediaDevices.getUserMedia(constraints)
   .then(stream => {
     video.srcObject = stream;
   })
   .catch(err => {
     alert("Gagal akses kamera: " + err.message);
+    console.error(err);
   });
